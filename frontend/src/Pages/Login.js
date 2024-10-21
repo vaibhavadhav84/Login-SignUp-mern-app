f306a6215bfdf2b948eda2ce32a57dc68ed5043b
Vaibhav Gajendra Adhav <154625347+vaibhavadhav84@users.noreply.github.com>
2024-10-21 19:06:36 +0530

    Update Login.js

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -26,7 +26,7 @@ function Login(){
             return handleError('email and password are required');
         }  
         try{
-            const url ="http://localhost:8080/auth/login";
+            const url ="https://login-sign-up-mern-app.vercel.app/auth/login";
             const response = await fetch(url,{
                 method:"POST",
                 headers:{
